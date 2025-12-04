26941e5d71b3f190f3be5f02b864fff18297a7bd
zzxxccaaqq223-pixel <zzxxccaaqq223@gmail.com>
2025-12-04 10:50:14 +0700

    Add various WMIC and system commands to script

--- Lab1-/wmic-command.cmd
+++ Lab1-/wmic-command.cmd
@@ -1,1 +1,13 @@
 dir
+wmic bios get serialnumber
+wmic nic get macaddress
+getmac
+wmic cpu
+wmic computersystem get totalphysicalmemory
+wmic partition get name,size,type
+wmic process list
+wmic product
+vol c:
+winver
+chkdsk
+systeminfo
